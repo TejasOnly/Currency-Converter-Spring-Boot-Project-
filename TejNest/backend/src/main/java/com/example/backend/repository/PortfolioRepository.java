9367ab9f3ec6d5c73c051bbc7bc0b65a5c7f72bd
TejasOnly <tejashande11@gmail.com>
2026-03-31 15:08:30 +0530

    Removed TejNest folder

--- TejNest/backend/src/main/java/com/example/backend/repository/PortfolioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.repository;
-
-import com.example.backend.entity.Portfolio;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
-    List<Portfolio> findByDesignerId(Long designerId);
-}
